14a29b032ddc9c6f957eed5c6ac3b41509791789
mgarciarelocity <michael.garcia@relocity.com>
2019-07-31 16:52:50 -0700

    added the ability to pass in an actionType and trigger a store action rather than mutating the state

--- test/utils/fake-publish.sh
+++ test/utils/fake-publish.sh
@@ -2,8 +2,8 @@
 
 npm pack ./
 rm -Rf ./test/package
-tar -xzf vuex-map-fields-*.tgz -C ./test
-rm -f vuex-map-fields-*.tgz
+tar -xzf relocity-vuex-map-fields-*.tgz -C ./test
+rm -f relocity-vuex-map-fields-*.tgz
 # Rename the fake published package to prevent Jest from
 # complaining about two packages having the same name.
-sed -i -e 's/vuex-map-fields/fake-vuex-map-fields/g' ./test/package/package.json
+sed -i -e 's/relocity-vuex-map-fields/fake-vuex-map-fields/g' ./test/package/package.json
